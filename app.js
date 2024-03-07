9dcb6a9ab9c0f6a5bb2b9cf276e959adcffac362
Dinesh Saini <sainidinesh2894@gmail.com>
2024-03-07 20:44:19 +0530

    create database variable

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const DATABASE = "wanderlust";
+const MONGO_URL = `mongodb://127.0.0.1:27017/${DATABASE}`;
 
 main().then(() => {
     console.log("connected to DB");
