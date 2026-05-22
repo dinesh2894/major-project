52b050d867ca3a919c7e1bb17e551f44e659f77f
Dinesh Saini <dk13722.biet@gmail.com>
2026-05-22 11:17:42 +0530

    add .env file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const {engine} = require("express/lib/application");
 const ExpressError = require("./utils/ExpressError.js");
-
+require('dotenv').config();
 const listings = require("./routes/listing.route.js");
 const reviews = require("./routes/review.route.js");
 
@@ -21,7 +21,6 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 // const DATABASE = "wanderlust";
 // const MONGO_URL = `mongodb://127.0.0.1:27017/${DATABASE}`;
-const MONGO_URL = `mongodb://dk13722biet:Spider9772140103@ac-fswvm6w-shard-00-00.hlhf5fz.mongodb.net:27017,ac-fswvm6w-shard-00-01.hlhf5fz.mongodb.net:27017,ac-fswvm6w-shard-00-02.hlhf5fz.mongodb.net:27017/?ssl=true&replicaSet=atlas-8vuhco-shard-0&authSource=admin&appName=Cluster0`;
 
 main().then(() => {
     console.log("connected to DB");
@@ -30,7 +29,7 @@ main().then(() => {
 });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
 }
 
 // root
